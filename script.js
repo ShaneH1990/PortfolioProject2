31b6d89d214516e2a5a74e516445369190762e4e
ShaneH1990 <shaneharraghy@live.ie>
2022-06-12 20:17:24 +0000

    buttons now remove and add data to page and answers are highlighted when hovered

--- script.js
+++ script.js
@@ -1,6 +1,59 @@
-let answerButton = document.getElementById("bncolor");
-answerButton.addEventListener('click', bnFunction);
+//Adds a eventListener to main document elements,It will listen for the DOM Content to be loaded then it will execute.
+// you should wait for the DOM to finish loading elements, otherwise you could be targeting elements that havent yet loaded! 
+document.addEventListener("DOMContentLoaded", function(){
+    
+    //byTagname is used to get all the buttons created in HTML
+    let buttons = document.getElementsByTagName("button");
+    
+    
 
-function bnFunction(event){
-    console.log("button pressed!");
-}
+    // this for loop will go through our buttons array and return each element in the array which will be stored in  that variable button on each iteration. 
+    for(let button of buttons){
+        //add eventListner for that button to be clicked when it is clicked the code inside the function will then run!
+        button.addEventListener("click",function(){
+            //if statment that checks the attribute of the data type to see what its value is and if its 'submit' it displays an alert!
+            //this. refers to the button that was just clicked!
+            if(this.getAttribute("data-type")=== "submit"){
+                //alert("please enter username");
+                createNew(); 
+                pageInfo();
+            }else if(this.getAttribute("data-type")=== "button"){
+               loginInfo();
+                //alert("You pressed the 'Next' Button!");
+           }
+            else{
+                let normalButton = this.getAttribute("data-type");
+            }
+        })
+        
+        //This function will display the image that is previously blocked by css rules when the submit button is pressed.
+        function createNew(){
+            
+            document.getElementById('p2').style.display = "block";
+            document.getElementById('unorganised1').style.display = "block";
+            document.getElementById('p3').style.display = "block";
+            document.getElementById('unorganised2').style.display = "block";
+            document.getElementById('p4').style.display = "block";
+            document.getElementById('unorganised3').style.display = "block";
+            document.getElementById('intro').style.display = "none";
+            document.getElementById('labelInfo').style.display = "none";
+            document.getElementById('userInfo').style.display = "none";
+        }
+        function pageInfo(){
+            document.getElementById('pageNumber').style.display = "block"; 
+        }
+        function loginInfo(){
+           alert("button works!");
+        }
+       // let playerArray = ["rock.jpg", "scissor.jpg","paper.jpg", "spock.jpg","lizard.jpg"];
+       // let playerLength = imageArray.length;
+
+        //let computerArray =["rock.jpg", "scissor.jpg", "paper.jpg", "spock.jpg", "lizard.jpg"];
+        //let computerLength = computerArray.length;
+
+        //function randomiseImg(){
+        //    let randompic = Math.floor(Math.random() *imageLength);
+        //}
+        
+    }
+})
