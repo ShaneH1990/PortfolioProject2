18bdcee46c6070d7f7497a7acdafd16d0fb1eb57
ShaneH1990 <shaneharraghy@live.ie>
2022-06-13 22:43:23 +0000

    page 3 displays and answers highlighted

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ document.addEventListener("DOMContentLoaded", function(){
     let buttons = document.getElementsByTagName("button");
     
     
+    
 
     // this for loop will go through our buttons array and return each element in the array which will be stored in  that variable button on each iteration. 
     for(let button of buttons){
@@ -13,17 +14,21 @@ document.addEventListener("DOMContentLoaded", function(){
         button.addEventListener("click",function(){
             //if statment that checks the attribute of the data type to see what its value is and if its 'submit' it displays an alert!
             //this. refers to the button that was just clicked!
-            if(this.getAttribute("data-type")=== "submit"){
+            if(this.getAttribute("data-type")=== "begin"){
                 //alert("please enter username");
-                createNew(); 
+                createNew();
+                loginCheck(); 
+                userName();
             }else if(this.getAttribute("data-type")==="next"){
                 pageTwo();
             
-            }else if(this.getAttribute("data-type")=== "button"){
+            }else if(this.getAttribute("data-type")=== "save"){
                loginInfo();
                 //alert("You pressed the 'Next' Button!");
-           }
-            else{
+            }else if(this.getAttribute("data-type")=== "next2"){
+                pageThree();
+            
+            }else{
                 let normalButton = this.getAttribute("data-type");
             }
         })
@@ -31,61 +36,113 @@ document.addEventListener("DOMContentLoaded", function(){
             let fixed = document.getElementById('mi');
             fixed.style.backgroundColor = "greenyellow";
         }
+            let firstanswer = document.getElementById('mi');
+            firstanswer.addEventListener("click", function () {
+            alert("you selected Mission Impossible!");
+            });
+
+        
         //This function will display the image that is previously blocked by css rules when the submit button is pressed.
         function createNew(){
             
-            document.getElementById('p2').style.display = "block";
+            document.getElementsByClassName('p2')[0].style.display = "block";
             document.getElementById('unorganised1').style.display = "block";
-            document.getElementById('p3').style.display = "block";
+            document.getElementsByClassName('p2')[1].style.display = "block";
             document.getElementById('unorganised2').style.display = "block";
-            document.getElementById('p4').style.display = "block";
+            document.getElementsByClassName('p2')[2].style.display = "block";
             document.getElementById('unorganised3').style.display = "block";
             document.getElementById('intro').style.display = "none";
             document.getElementById('labelInfo').style.display = "none";
+            document.getElementById('bncolor').style.display = "none";
             document.getElementById('userInfo').style.display = "none";
             document.getElementById('login').style.display = "none";
-            document.getElementById('p5').style.display = "none";
+            document.getElementsByClassName('p2')[3].style.display = "none";
             document.getElementById('unorganised4').style.display = "none";
-            document.getElementById('p6').style.display = "none";
+            document.getElementsByClassName('p2')[4].style.display = "none";
             document.getElementById('unorganised5').style.display = "none";
-            document.getElementById('p7').style.display = "none";
+            document.getElementsByClassName('p2')[5].style.display = "none";
             document.getElementById('unorganised6').style.display = "none";
             document.getElementById('intro').style.display = "none";
             document.getElementById('labelInfo').style.display = "none";
-            document.getElementById('bncolor').style.display = "none";
             document.getElementById('next1').style.display = "block";
+            document.getElementById('usernamedisplay').style.display = "block";
+            
+            
         }
-        
+       
         function pageTwo(){
 
-            document.getElementById('p2').style.display = "none";
+            document.getElementsByClassName('p2')[0].style.display = "none";
             document.getElementById('unorganised1').style.display = "none";
-            document.getElementById('p3').style.display = "none";
+            document.getElementsByClassName('p2')[1].style.display = "none";
             document.getElementById('unorganised2').style.display = "none";
-            document.getElementById('p4').style.display = "none";
+            document.getElementsByClassName('p2')[2].style.display = "none";
             document.getElementById('intro').style.display = "none";
             document.getElementById('labelInfo').style.display = "none";
             document.getElementById('userInfo').style.display = "none";
             document.getElementById('login').style.display = "none";
             document.getElementById('unorganised3').style.display = "none";
-            document.getElementById('p5').style.display = "block";
+            document.getElementsByClassName('p2')[3].style.display = "block";
             document.getElementById('unorganised4').style.display = "block";
-            document.getElementById('p6').style.display = "block";
+            document.getElementsByClassName('p2')[4].style.display = "block";
             document.getElementById('unorganised5').style.display = "block";
-            document.getElementById('p7').style.display = "block";
+            document.getElementsByClassName('p2')[5].style.display = "block";
             document.getElementById('unorganised6').style.display = "block";
-            document.getElementById('intro').style.display = "block";
-            document.getElementById('labelInfo').style.display = "block";
-            document.getElementById('bncolor').style.display = "block";
+            document.getElementById('intro').style.display = "none";
+            document.getElementById('labelInfo').style.display = "none";
+            document.getElementById('bncolor').style.display = "none";
 
             document.getElementById('next1').style.display = "block";
+            document.getElementById('next2').style.display = "block";
 
         }
+
+        function pageThree(){
+            document.getElementsByClassName('p2')[0].style.display = "none";
+            document.getElementById('unorganised1').style.display = "none";
+            document.getElementsByClassName('p2')[1].style.display = "none";
+            document.getElementById('unorganised2').style.display = "none";
+            document.getElementsByClassName('p2')[2].style.display = "none";
+            document.getElementById('intro').style.display = "none";
+            document.getElementById('labelInfo').style.display = "none";
+            document.getElementById('userInfo').style.display = "none";
+            document.getElementById('login').style.display = "none";
+            document.getElementById('unorganised3').style.display = "none";
+            document.getElementsByClassName('p2')[3].style.display = "none";
+            document.getElementById('unorganised4').style.display = "none";
+            document.getElementsByClassName('p2')[4].style.display = "none";
+            document.getElementById('unorganised5').style.display = "none";
+            document.getElementsByClassName('p2')[5].style.display = "none";
+            document.getElementById('unorganised6').style.display = "none";
+            document.getElementById('intro').style.display = "none";
+            document.getElementById('labelInfo').style.display = "none";
+            document.getElementById('bncolor').style.display = "none";
+            document.getElementById('next1').style.display = "none";
+            document.getElementsByClassName('p2')[6].style.display = "block";
+            document.getElementById('unorganised7').style.display = "block";
+            document.getElementsByClassName('p2')[7].style.display = "block";
+            document.getElementById('unorganised8').style.display = "block";
+            document.getElementsByClassName('p2')[8].style.display = "block";
+            document.getElementById('unorganised9').style.display = "block";
+        }
+
 
         function loginInfo(){
            let getinfo = document.getElementById('userInfo').value;
            alert("Welcome " + getinfo);
         }
-       
+        function userName(){
+            let name = document.getElementById('login').value;
+            document.getElementById('usernamedisplay').innerHTML = name;
+
+        }
+
+        function loginCheck(){
+           let info = document.getElementById('userInfo').value;
+           if(info===""){
+               alert("please enter name first!");
+               let entry = document.getElementById("bncolor").style.display = "block";
+           }
+       }
     }
 })
